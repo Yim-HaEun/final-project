204dce0430ddb40ca2aabe4661c657b6108c33f1
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-01-24 19:00:25 +0900

    kakao marker

--- client/src/main/KakaoMap.js
+++ client/src/main/KakaoMap.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/KakaoMap.css';
 import usersUserinfoAxios from '../token/tokenAxios';
 const KakaoMap = () => {
-  const [userData, setUserData] = useState([]);
+  const [userData, setUserData] = useState([]); //주소값
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         // 서버에 사용자 정보를 가져오는 요청
         const response = await usersUserinfoAxios.get('/users/userinfo');
-        setUserData(response.data);
+        setUserData(response.data); // 로그인한 토큰 이용해서 해당 유저 데이터 가져오는거
         console.log(userData);
       } catch (error) {
         console.error('Failed to fetch user data.', error);
@@ -32,24 +33,26 @@ const KakaoMap = () => {
 
         const mapOption = {
           //맵
-          center: new window.kakao.maps.LatLng(37.502, 127.026581),
+          center: new window.kakao.maps.LatLng(37.5665, 126.978), // 지도의 중심좌표가 나의 집이 되어야함
           level: 4,
         };
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
-        const geocoder = new window.kakao.maps.services.Geocoder(); //입력한 주소에 마커
+        const geocoder = new window.kakao.maps.services.Geocoder(); //주소- 좌표 변환 객체
+        // userData.useraddress 로그인한 유저의 DB에저장된 Address 부분
         geocoder.addressSearch(userData.useraddress, (result, status) => {
           if (status === window.kakao.maps.services.Status.OK) {
+            //정상적으로 검색이 완료 되면
             const coords = new window.kakao.maps.LatLng(
               result[0].y,
               result[0].x
-            );
+            ); //좌표 받기
 
             const marker = new window.kakao.maps.Marker({
               //마커로 표시
               map: map,
               position: coords,
             });
-            // 장소에 대한 설명을 표시합니다
+            //infowindow로 장소에 대한 설명을 표시합니다
             const infowindow = new window.kakao.maps.InfoWindow({
               content:
                 '<div style="width:150px;text-align:center;padding:6px 0;">나의 집</div>',
@@ -58,55 +61,9 @@ const KakaoMap = () => {
             map.setCenter(coords);
           }
         });
-
-        var content =
-          '<div class="overlaybox">' +
-          '    <div class="boxtitle">금주 영화순위</div>' +
-          '    <div class="first">' +
-          '        <div class="triangle text">1</div>' +
-          '        <div class="movietitle text">드래곤 길들이기2</div>' +
-          '    </div>' +
-          '    <ul>' +
-          '        <li class="up">' +
-          '            <span class="number">2</span>' +
-          '            <span class="title">명량</span>' +
-          '            <span class="arrow up"></span>' +
-          '            <span class="count">2</span>' +
-          '        </li>' +
-          '        <li>' +
-          '            <span class="number">3</span>' +
-          '            <span class="title">해적(바다로 간 산적)</span>' +
-          '            <span class="arrow up"></span>' +
-          '            <span class="count">6</span>' +
-          '        </li>' +
-          '        <li>' +
-          '            <span class="number">4</span>' +
-          '            <span class="title">해무</span>' +
-          '            <span class="arrow up"></span>' +
-          '            <span class="count">3</span>' +
-          '        </li>' +
-          '        <li>' +
-          '            <span class="number">5</span>' +
-          '            <span class="title">안녕, 헤이즐</span>' +
-          '            <span class="arrow down"></span>' +
-          '            <span class="count">1</span>' +
-          '        </li>' +
-          '    </ul>' +
-          '</div>';
-
-        const position = new window.kakao.maps.LatLng(37.49887, 127.026581);
-
-        const customOverlay = new window.kakao.maps.CustomOverlay({
-          position: position,
-          content: content,
-          xAnchor: 0.3,
-          yAnchor: 0.91,
-        });
-
-        customOverlay.setMap(map);
       });
     };
-  }, []);
+  });
 
   return (
     <div className="container">
