e7834f6bd759439d732f0da5ab9258af3a14a045
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-01-30 20:00:10 +0900

    modal

--- Swith/src/main/java/lm/swith/studyroom/controller/StudyRoomController.java
+++ Swith/src/main/java/lm/swith/studyroom/controller/StudyRoomController.java
@@ -1,31 +1,37 @@
 package lm.swith.studyroom.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lm.swith.studyroom.model.StudyMoment;
 import lm.swith.studyroom.model.StudyRoomNotice;
 import lm.swith.studyroom.service.StudyRoomService;
 import lombok.RequiredArgsConstructor;
 
-//@RestController
-@Controller
+@RestController
 @RequestMapping("/studyRoom")
 @RequiredArgsConstructor
-@CrossOrigin(origins="http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins="http://localhost:3000", allowCredentials = "true", allowedHeaders="*")
 public class StudyRoomController {
 	private final StudyRoomService studyRoomService;
 
 	// StudyRoomNotice
 	@PostMapping("/create/StudyNoticeMoment/{post_no}") // INSERT
-	public ResponseEntity<?> createStudyRoomNotice(@PathVariable Long post_no, @RequestBody StudyRoomNotice studyRoomNotice){
-		studyRoomService.createStudyRoomNotice(studyRoomNotice);
-		return ResponseEntity.ok("Success");
+	public ResponseEntity<?> createStudyRoomNotice(@PathVariable Long post_no, @RequestBody StudyRoomNotice notice){
+		
+		
+		System.out.println("getPost_no : " +  notice.getPost_no());
+		System.out.println("getUser_no : " + notice.getUser_no());
+		System.out.println("getNotice_title : " + notice.getNotice_title());
+		System.out.println("getNotice_content : " + notice.getNotice_content());
+		System.out.println("getNotice_password : " + notice.getNotice_password());
+		studyRoomService.createStudyRoomNotice(notice);
+		return ResponseEntity.ok(studyRoomService);
 	}
 	
 	// Study Moment
