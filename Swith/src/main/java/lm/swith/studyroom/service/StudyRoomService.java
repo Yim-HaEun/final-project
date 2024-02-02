d07688d5f44923d3bd60bb1ffa43a95f43320041
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-02-02 12:28:02 +0900

    moment

--- Swith/src/main/java/lm/swith/studyroom/service/StudyRoomService.java
+++ Swith/src/main/java/lm/swith/studyroom/service/StudyRoomService.java
@@ -20,8 +20,11 @@ public class StudyRoomService {
 		studyRoomMapper.createStudyMoment(studyMoment);
 	}
 	
-	public void findByStudyMoment(StudyMoment studyMoment) {
-		studyRoomMapper.findByStudyMoment(studyMoment);
+	public  List<StudyMoment> findByStudyMoment(Long post_no) {
+		return studyRoomMapper.findByStudyMoment(post_no);
+	}
+	public void deleteStudyMoment(Long moment_no, Long user_no) {
+		studyRoomMapper.deleteStudyMoment(moment_no, user_no);
 	}
 	
 	
@@ -32,8 +35,8 @@ public class StudyRoomService {
 	public void createStudyRoomNotice(StudyRoomNotice studyRoomNotice) {
 		studyRoomMapper.createStudyRoomNotice(studyRoomNotice);
 	}
-	public List<StudyRoomNotice> findByStudyRoomNotice(Long post_no) {
-		return studyRoomMapper.findByStudyRoomNotice(post_no);
+	public List<StudyRoomNotice> findByStudyNoticeWithNickname(Long post_no){
+		return studyRoomMapper.findByStudyNoticeWithNickname(post_no);
 	}
 	public void deleteStudyRoomNotice(Long notice_no, String notice_password) {
 		studyRoomMapper.deleteStudyRoomNotice(notice_no, notice_password);
