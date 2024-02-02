36fcde766f75c6cd9bd63c594ec34a51913b0e42
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-02-02 12:27:13 +0900

    moment

--- Swith/src/main/java/lm/swith/studyroom/model/StudyMoment.java
+++ Swith/src/main/java/lm/swith/studyroom/model/StudyMoment.java
@@ -1,6 +1,8 @@
 package lm.swith.studyroom.model;
 
-import java.util.Date;
+
+
+import java.sql.Date;
 
 import lombok.*;
 
@@ -13,7 +15,11 @@ public class StudyMoment {
 	private Long post_no;
 	private Long user_no;
 	private byte[] moment_picture;
+	private String img;
 	private String moment_title;
+	private String nickname;
 	private Date moment_post_date;
-
+	
 }
+
+
