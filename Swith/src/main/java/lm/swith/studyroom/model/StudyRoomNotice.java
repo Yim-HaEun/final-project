b0b440222537df763cc2806d382907af9771fea8
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-02-02 12:27:39 +0900

    moment

--- Swith/src/main/java/lm/swith/studyroom/model/StudyRoomNotice.java
+++ Swith/src/main/java/lm/swith/studyroom/model/StudyRoomNotice.java
@@ -17,9 +17,11 @@ public class StudyRoomNotice {
 	private Long notice_no;
 	private Long post_no;
 	private Long user_no;
+	private String nickname;
 	private String notice_title;
 	private String notice_content;
 	private String notice_password;
 	private Date notice_post_date;
+	
 
 }
