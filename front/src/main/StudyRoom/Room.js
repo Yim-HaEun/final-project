1348e0b86a0d406fbffcdc5ee96922bf6881f3d5
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-02-03 18:21:41 +0900

    'dday'

--- front/src/main/StudyRoom/Room.js
+++ front/src/main/StudyRoom/Room.js
@@ -1,13 +1,14 @@
 import Header from '../Header';
 import usersUserinfoAxios from '../../token/tokenAxios';
-
+import Chat from './Chat';
 import React, { useState, useEffect } from 'react';
 import { useParams, BrowserRouter, Routes, Route } from 'react-router-dom';
 import NoticeModal from './NoticeModal';
 import MomentModal from './MomentModal';
 import Notice from './Notice';
 import Moment from './Moment';
-function StudyRoom() {
+import DayCount from './DayCount';
+const StudyRoom = () => {
   const [userData, setUserData] = useState('');
   const [notice, setNotice] = useState({
     post_no: '',
@@ -32,21 +33,6 @@ function StudyRoom() {
   }, []);
 
   const { post_no } = useParams(); // 동적 라우트 매개변수 가져오기
-  const [studyRoomPages, setStudyRoomPages] = useState([]);
-
-  useEffect(() => {
-    const fetchStudyDetail = async () => {
-      try {
-        const response = await usersUserinfoAxios.get(`/study_room/${post_no}`);
-        setStudyRoomPages(response.data);
-        console.log(studyRoomPages);
-      } catch (error) {
-        console.log('Error fetching study detail: ', error);
-      }
-    };
-
-    fetchStudyDetail();
-  }, [post_no]); // post_no가 변경될 때마다 실행
 
   return (
     <div>
@@ -55,15 +41,32 @@ function StudyRoom() {
         <h4>Notice</h4>
         {/*post_no, user_no */}
         <p>하냥이의 번호는 = {post_no}</p>
+
         <div>
+          <DayCount />
+          <br />
+          <br />
+          <br />
+          <br />
           <NoticeModal />
+          <br />
+
           <Notice />
+          <br />
+          <br />
+          <br />
+          <br />
           <MomentModal />
           <Moment />
         </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <Chat />
       </div>
     </div>
   );
-}
+};
 
 export default StudyRoom;
