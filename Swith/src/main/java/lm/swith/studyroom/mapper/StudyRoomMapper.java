9891a28162dd10e7e1e7ce5172e92ca7937975ab
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2024-02-03 18:18:33 +0900

    'dday'

--- Swith/src/main/java/lm/swith/studyroom/mapper/StudyRoomMapper.java
+++ Swith/src/main/java/lm/swith/studyroom/mapper/StudyRoomMapper.java
@@ -2,8 +2,12 @@ package lm.swith.studyroom.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
+import lm.swith.studyroom.model.MessageRequestDto;
 import lm.swith.studyroom.model.StudyMoment;
 import lm.swith.studyroom.model.StudyRoomNotice;
 
@@ -29,6 +33,16 @@ public interface StudyRoomMapper {
 	
 	//DELETE
 	void deleteStudyMoment(Long moment_no, Long user_no);//생각해보기
+	
+	
+// Chatting
+ 	@Insert("INSERT INTO chat_messages (user_id, room_id, message, timestamp) " +
+            "VALUES (#{userId}, #{post_no}, #{message}, #{timestamp})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void saveChatMessage(MessageRequestDto chatMessage);
+
+    @Select("SELECT * FROM chat_messages WHERE post_no = #{post_no} ORDER BY timestamp ASC")
+    List<MessageRequestDto> getChatMessagesByRoomId(Long post_no);
 }
 	
 	
